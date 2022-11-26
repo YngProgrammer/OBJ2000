9a9b0bd3c08367938afa8d04ca3b852a4c85c742
YngProgrammer <antonin.dejesus@outlook.com>
2022-11-26 10:51:47 +0100

    Answer to exercise 6, question 1)

---
+++ eksamen/2020-12/oppgave6/Docs.java
@@ -0,0 +1,40 @@
+package oppgave6;
+
+public class Docs {
+    private int archiveNumber;
+    private String title;
+    private String subtitle;
+    private int year;
+
+    public int getArchiveNumber() {
+        return this.archiveNumber;
+    }
+
+    public void setArchiveNumber(int number) {
+        this.archiveNumber = number;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return this.subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
