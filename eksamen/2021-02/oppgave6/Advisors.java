a60430a3b7c9072529d26d8aff3131bd1bc8dd42
YngProgrammer <antonin.dejesus@outlook.com>
2022-11-26 15:10:06 +0100

    Answer to exercise 6, question 10) (2021)

--- eksamen/2021-02/oppgave6/Advisors.java
+++ eksamen/2021-02/oppgave6/Advisors.java
@@ -2,4 +2,18 @@ package oppgave6;
 
 public class Advisors extends Employees {
     public String department;
+
+    public void printAdvisor() {
+        System.out.println("First name: " + getFirstName());
+        System.out.println("Last name: " + getLastName());
+    }
+
+    public void printFullAdvisor() {
+        printAdvisor();
+
+        System.out.println("Employee number: " + getEmployeeNumber());
+        System.out.println("Age: " + getAge());
+
+        System.out.println("Department: " + department);
+    }
 }
