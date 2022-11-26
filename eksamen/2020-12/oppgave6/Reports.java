c9e27e29777a5fafb2bf5dcb1b87371928114d65
YngProgrammer <antonin.dejesus@outlook.com>
2022-11-26 11:12:11 +0100

    Answer to exercise 6, question 6)

--- eksamen/2020-12/oppgave6/Reports.java
+++ eksamen/2020-12/oppgave6/Reports.java
@@ -3,4 +3,25 @@ package oppgave6;
 public class Reports extends Docs {
     private int department;
     private boolean confidential;
+
+    public Reports(
+        int number, 
+        String title,
+        int department, 
+        boolean confidential) {
+        super(number, title);
+        this.department = department;
+        this.confidential = confidential;
+    }
+    
+    public Reports(
+        int number, 
+        String title,
+        String subtitle,
+        int department, 
+        boolean confidential) {
+        super(number, title, subtitle);
+        this.department = department;
+        this.confidential = confidential;
+    }
 }
