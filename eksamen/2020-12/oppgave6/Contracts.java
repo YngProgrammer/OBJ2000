bdcbc368eb32d0ca4d37337b0e0de37d8b8b4b5c
YngProgrammer <antonin.dejesus@outlook.com>
2022-11-26 11:28:29 +0100

    Answer to exercise 6, question 10)

--- eksamen/2020-12/oppgave6/Contracts.java
+++ eksamen/2020-12/oppgave6/Contracts.java
@@ -2,4 +2,21 @@ package oppgave6;
 
 public class Contracts extends Docs{
     private String partner;
+
+    public Contracts(
+        int number, 
+        String title,
+        String partner) {
+        super(number, title);
+        this.partner = partner;
+    }
+    
+    public Contracts(
+        int number, 
+        String title,
+        String subtitle,
+        String partner) {
+        super(number, title, subtitle);
+        this.partner = partner;
+    }
 }
