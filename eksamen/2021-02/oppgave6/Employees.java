0d28e4afcd8eef2f74ecb9c835820b9ca7bfcb3a
YngProgrammer <antonin.dejesus@outlook.com>
2022-11-26 14:33:39 +0100

    Answer to exercise 6, question 1) (2021)

---
+++ eksamen/2021-02/oppgave6/Employees.java
@@ -0,0 +1,40 @@
+package oppgave6;
+
+public class Employees {
+    private int employeeNumber;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public int getEmployeeNumber() {
+        return this.employeeNumber;
+    }
+
+    public void setEmployeeNumber(int number) {
+        this.employeeNumber = number;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
